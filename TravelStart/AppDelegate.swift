fb3101887a7fcc6a3f2bdde5f7365aae9b6d4915
hsiehkl <st4465@gmail.com>
2018-09-21 19:58:56 +0800

    delete space pr

--- TravelStart/AppDelegate.swift
+++ TravelStart/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         setupRootViewController()
         setupNaviTheme()
         setupThirdPartySettings()
-    
+
         return true
     }
 }
